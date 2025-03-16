b405a23fdca76acbe97ec6cac141dd470542033e
SuperNova74-k <kingkhaled.w@gmail.com>
2025-03-16 06:23:31 +0200

    Embeddings

--- TFIDF.py
+++ TFIDF.py
@@ -1,6 +1,6 @@
 import math
 import string
-
+import numpy as np
 
 def _dictionarize_documents(documents):
     documents_frequencies = []
@@ -40,6 +40,11 @@ class TF_IDF:
 
         self._documents_frequencies = _dictionarize_documents(self._documents)
 
+        self._vocab = set()
+        for document in self._documents:
+            self._vocab.update(document)
+        self._vocab = sorted(list(self._vocab))
+
     def tf(self, word, document_index):
         return self._documents_frequencies[document_index].get(word, 0)
 
@@ -52,3 +57,30 @@ class TF_IDF:
 
     def tfidf(self, word, document_index):
         return self.tf(word, document_index) * self.idf(word)
+
+    def tfidf_all_words(self, document_index, normalized:bool ,limit=None):
+        word_scores = [(word, self.tfidf(word, document_index)) for word in self._documents[document_index]]
+
+        scores = [word_score[1] for word_score in word_scores]
+
+        if normalized:
+            norm = np.linalg.norm(scores)
+            scores = scores / norm
+
+        indexes = np.argsort(scores)[::-1]
+        word_scores = [word_scores[i] for i in indexes]
+
+        if limit:
+            word_scores = word_scores[:limit]
+
+        return word_scores
+
+    def word_embedding(self, word):
+        return np.array([self.tfidf(word=word, document_index=i) for i in range(len(self._documents))])
+
+    def document_embedding(self, document_index, normalized:bool):
+        document = np.array([self.tfidf(word=word, document_index=document_index) for word in self._vocab])
+        if normalized:
+            norm = np.linalg.norm(document)
+            document = document / norm
+        return document
